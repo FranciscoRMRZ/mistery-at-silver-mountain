679bacc31008e163f19b16355ba53725995d9a5b
Francisco Ramirez <py.francisco.ramirez@gmail.com>
2024-04-29 11:38:11 -0600

    Add reference book and base files

---
+++ src/main.py
@@ -0,0 +1,24 @@
+# this is a try to port 'Adventure in silver Mountain' to Python
+# from the book of the same name
+def func_3380():
+    pass
+
+def func_4400():
+    pass
+
+def func_3310():
+    pass
+
+el = 39
+no = 88
+nv = 57
+g = 28
+
+func_3380()
+func_4400()
+
+ll = 0
+
+func_3310()
+
+P = x(
